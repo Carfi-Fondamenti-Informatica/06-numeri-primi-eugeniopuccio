f3e76ef288f9dc9ef0b2b51ea1531bd51cbdb67d
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-03 11:50:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
 int main() {
+
     int a;
-    bool b;
-
     cin>>a;
-    b=primi(a);
-
-    if (b==true)
-        cout<<"numero primo"<<endl;
-    else if (b==false)
-        cout<<"numero non primo"<<endl;
+    if(nprimo(a) == true ){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
     return 0;
 }
