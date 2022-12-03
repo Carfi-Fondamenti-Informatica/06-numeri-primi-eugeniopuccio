e3b71052084f8b8da6f67be7fc974d2e3602eb88
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-03 11:52:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool nprimo(int a) {
+    int i=1, n=a;
+    if (a != 1) {
+        do{
+            i++;
+            n=a%i;
+        }while (n);
+    }else{
+        return true;
+    }
+    if (i==a){
+        return true;
+    }else{
+        return false;
+    }
+}
